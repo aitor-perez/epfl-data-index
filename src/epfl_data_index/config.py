390a04c014e16a0120e02655e8d569677c6b41b5
Aitor Pérez <aitor.perez@epfl.ch>
2026-06-24 12:00:41 +0200

    :egg: fail if no env vars

--- src/epfl_data_index/config.py
+++ src/epfl_data_index/config.py
@@ -3,7 +3,10 @@ import os
 
 class _Config:
     def __getitem__(self, key):
-        return os.environ.get(key)
+        try:
+            return os.environ[key]
+        except KeyError:
+            raise KeyError(f"Missing required environment variable: {key}") from None
 
 
 CONFIG = _Config()
